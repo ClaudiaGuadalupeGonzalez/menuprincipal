4e03cfba24141d8dc36f4a54efef0bc76aeb39ad
ClaudiaGuadalupeGonzalez <guzmanuni96@hotmail.com>
2019-10-07 10:02:25 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/p3cggg/fRMpRINCIPALActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.itics.p3cggg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fRMpRINCIPALActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_rmp_rincipal);
+    }
+}
